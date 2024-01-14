6b8c56c6a3620ec509b06c037808875d58a4f6e9
Lakshminarayana008 <156606290+Lakshminarayana008@users.noreply.github.com>
2024-01-14 15:02:39 -0600

    Delete ICP_1_Assignment directory

--- ICP_1_Assignment/neural2.py
+++
@@ -1,8 +0,0 @@
-X = float(input("Enter first number:"))
-Y = float(input("Enter second number:"))
-
-# operations for + - / * are done in the print statements
-print(X, "+", Y, "=", X+Y)
-print(X, "-", Y, "=", X-Y)
-print(X, "/", Y, "=", X/Y)
-print(X, "*", Y, "=", X*Y)
