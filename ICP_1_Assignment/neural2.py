5d9188d7f7cd4b84ec1eb7cd7cc2c490733397cd
Lakshminarayana008 <156606290+Lakshminarayana008@users.noreply.github.com>
2024-01-14 14:34:15 -0600

    Add files via upload

---
+++ ICP_1_Assignment/neural2.py
@@ -0,0 +1,8 @@
+X = float(input("Enter first number:"))
+Y = float(input("Enter second number:"))
+
+# operations for + - / * are done in the print statements
+print(X, "+", Y, "=", X+Y)
+print(X, "-", Y, "=", X-Y)
+print(X, "/", Y, "=", X/Y)
+print(X, "*", Y, "=", X*Y)
